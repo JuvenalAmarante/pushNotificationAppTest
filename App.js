67c1e8dfb23be5ce7d08f25e6f0b48891af3d6c7
Juvenal Amarante <juvenalamarante13@gmail.com>
2019-10-28 11:31:29 -0300

    Atualizando estilos da pagina

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React, {useEffect} from 'react';
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 import PushNotification from 'react-native-push-notification';
 import OneSignal from 'react-native-onesignal';
 
 export default function App() {
   OneSignal.init('1fe3def1-d66b-4b29-bd49-f32e6ce26b8d');
+  OneSignal.enableVibrate(true);
+  OneSignal.addTrigger('hour', 11);
 
   useEffect(() => {
     PushNotification.configure({
@@ -33,41 +42,50 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Bem vindo ao Push Notification</Text>
+    <LinearGradient colors={['#4834d4', '#1e136e']} style={styles.container}>
+      <StatusBar
+        backgroundColor="transparent"
+        translucent
+        barStyle="light-content"
+      />
+      <View style={styles.title}>
+        <Text style={styles.titleText}>Push Notification</Text>
+        <Text style={styles.titleText}>Bem vindo</Text>
+      </View>
       <TouchableOpacity
         style={styles.button}
         onPress={() => {
           notification();
         }}>
-        <Text style={styles.instructions}>
+        <Text style={styles.textButton}>
           Aperte para exibir uma notificação
         </Text>
       </TouchableOpacity>
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center',
     backgroundColor: '#4834d4',
   },
-  welcome: {
-    fontSize: 20,
+  title: {},
+  titleText: {
+    fontSize: 40,
     color: 'white',
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
+  textButton: {
+    fontSize: 18,
     textAlign: 'center',
     color: 'white',
     marginBottom: 5,
   },
   button: {
-    // backgroundColor: '#E5ECEE',
     padding: 15,
     borderWidth: 1,
     borderColor: 'white',
